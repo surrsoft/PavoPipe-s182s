f484a3fa0d77786fbb665594b5ae8682d9ff8b5c
macprosha <evgebar-1@mail.ru>
2025-03-18 03:57:03 +0300

    next

--- src/utils/pavoPipeToJson.test.ts
+++ src/utils/pavoPipeToJson.test.ts
@@ -1,22 +1,11 @@
 import { pavoPipeToJson } from './pavoPipeToJson';
 
-describe('pavoPipeToJson', () => {
-  test('должен парсить PavoPipe формат с простыми k-item', () => {
-    const input = `- |n текст на английском языке |s текст на русском языке
+// Тестовые данные
+const SIMPLE_K_ITEM_INPUT = `- |n текст на английском языке |s текст на русском языке
 - |s стол |n table
 - |n bar |foo ещё что-то`;
 
-    const expected = [
-      { n: 'текст на английском языке', s: 'текст на русском языке' },
-      { s: 'стол', n: 'table' },
-      { n: 'bar', foo: 'ещё что-то' }
-    ];
-
-    expect(pavoPipeToJson(input)).toEqual(expected);
-  });
-
-  test('должен парсить PavoPipe формат с d-item и k-item', () => {
-    const input = `= c название столицы
+const D_ITEM_AND_K_ITEM_INPUT = `= c название столицы
 = f дата основания
 
 - |c Париж |f 3 век до н.э.
@@ -24,18 +13,7 @@ describe('pavoPipeToJson', () => {
 - |c Лондон |f ок. 47 год н.э.
 - |c Москва |f 1147 год`;
 
-    const expected = [
-      { c: 'Париж', f: '3 век до н.э.' },
-      { c: 'Рим', f: '753 год до н.э.' },
-      { c: 'Лондон', f: 'ок. 47 год н.э.' },
-      { c: 'Москва', f: '1147 год' }
-    ];
-
-    expect(pavoPipeToJson(input)).toEqual(expected);
-  });
-
-  test('должен парсить PavoPipe формат с переносами строк между полями', () => {
-    const input = `= c название   столицы
+const MULTILINE_FIELDS_INPUT = `= c название   столицы
 = f дата       основания
 
 - |c Париж
@@ -47,18 +25,7 @@ describe('pavoPipeToJson', () => {
 - |c Москва 
     |f 1147 год`;
 
-    const expected = [
-      { c: 'Париж', f: '3 век до н.э.' },
-      { c: 'Рим', f: '753 год до н.э.' },
-      { c: 'Лондон', f: 'ок. 47 год н.э.' },
-      { c: 'Москва', f: '1147 год' }
-    ];
-
-    expect(pavoPipeToJson(input)).toEqual(expected);
-  });
-
-  test('должен обрабатывать пустые строки и различные пробельные символы', () => {
-    const input = `
+const EMPTY_LINES_INPUT = `
     
 = c название столицы
 
@@ -68,16 +35,7 @@ describe('pavoPipeToJson', () => {
 - |c Рим |f 753 год до н.э.
 `;
 
-    const expected = [
-      { c: 'Париж', f: '3 век до н.э.' },
-      { c: 'Рим', f: '753 год до н.э.' }
-    ];
-
-    expect(pavoPipeToJson(input)).toEqual(expected);
-  });
-
-  test('должен обрабатывать сложные многострочные структуры', () => {
-    const input = `= name имя
+const COMPLEX_MULTILINE_INPUT = `= name имя
 = age возраст
 = job работа
 
@@ -90,12 +48,55 @@ describe('pavoPipeToJson', () => {
 - |name Алексей |age 40
   |job Менеджер`;
 
+describe('pavoPipeToJson', () => {
+  test('P01 - должен парсить PavoPipe формат с простыми k-item', () => {
+    const expected = [
+      { n: 'текст на английском языке', s: 'текст на русском языке' },
+      { s: 'стол', n: 'table' },
+      { n: 'bar', foo: 'ещё что-то' }
+    ];
+
+    expect(pavoPipeToJson(SIMPLE_K_ITEM_INPUT)).toEqual(expected);
+  });
+
+  test('P02 - должен парсить PavoPipe формат с d-item и k-item', () => {
+    const expected = [
+      { c: 'Париж', f: '3 век до н.э.' },
+      { c: 'Рим', f: '753 год до н.э.' },
+      { c: 'Лондон', f: 'ок. 47 год н.э.' },
+      { c: 'Москва', f: '1147 год' }
+    ];
+
+    expect(pavoPipeToJson(D_ITEM_AND_K_ITEM_INPUT)).toEqual(expected);
+  });
+
+  test('P03 - должен парсить PavoPipe формат с переносами строк между полями', () => {
+    const expected = [
+      { c: 'Париж', f: '3 век до н.э.' },
+      { c: 'Рим', f: '753 год до н.э.' },
+      { c: 'Лондон', f: 'ок. 47 год н.э.' },
+      { c: 'Москва', f: '1147 год' }
+    ];
+
+    expect(pavoPipeToJson(MULTILINE_FIELDS_INPUT)).toEqual(expected);
+  });
+
+  test('P04 - должен обрабатывать пустые строки и различные пробельные символы', () => {
+    const expected = [
+      { c: 'Париж', f: '3 век до н.э.' },
+      { c: 'Рим', f: '753 год до н.э.' }
+    ];
+
+    expect(pavoPipeToJson(EMPTY_LINES_INPUT)).toEqual(expected);
+  });
+
+  test('P05 - должен обрабатывать сложные многострочные структуры', () => {
     const expected = [
       { name: 'Иван', age: '30', job: 'Программист' },
       { name: 'Мария', age: '25', job: 'Дизайнер' },
       { name: 'Алексей', age: '40', job: 'Менеджер' }
     ];
 
-    expect(pavoPipeToJson(input)).toEqual(expected);
+    expect(pavoPipeToJson(COMPLEX_MULTILINE_INPUT)).toEqual(expected);
   });
 });
